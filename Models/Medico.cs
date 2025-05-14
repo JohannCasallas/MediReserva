8a65e693220ebc1be8e17ce28878d85799f88c07
Johann Casallas <casallasbecerrajohann@gmail.com>
2025-05-14 01:26:16 -0500

    Se implementa Entity Framework para conectar la base de datos

---
+++ Models/Medico.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MediReserva.Models;
+
+public partial class Medico
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string? Email { get; set; }
+
+    public string? Telefono { get; set; }
+
+    public string? Consultorio { get; set; }
+
+    public int EspecialidadId { get; set; }
+
+    public virtual ICollection<Citum> Cita { get; set; } = new List<Citum>();
+
+    public virtual Especialidad Especialidad { get; set; } = null!;
+}
---
+++ Models/Paciente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MediReserva.Models;
+
+public partial class Paciente
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Apellido { get; set; } = null!;
+
+    public string Documento { get; set; } = null!;
+
+    public string? Email { get; set; }
+
+    public string? Telefono { get; set; }
+
+    public DateOnly? FechaNacimiento { get; set; }
+
+    public virtual ICollection<Citum> Cita { get; set; } = new List<Citum>();
+}
