6c71f8108aafaaf1041f552506b431cf4573139d
Johann Casallas <casallasbecerrajohann@gmail.com>
2025-05-15 01:13:14 -0500

    Creacion de Meddleware para manejo de errores

---
+++ Middleware/Class.cs
@@ -0,0 +1,40 @@
+﻿using MediReserva.Models;
+
+namespace MediReserva.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+        public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error no manejado");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            var response = new ApiResponse<string>(null, false, "Ocurrió un error interno.");
+            var json = System.Text.Json.JsonSerializer.Serialize(response);
+            return context.Response.WriteAsync(json);
+        }
+    }
+
+}
