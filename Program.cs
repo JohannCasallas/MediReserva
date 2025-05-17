445a45dd90e12bc758243084ad724e5fbc74d4c5
Johann Casallas <casallasbecerrajohann@gmail.com>
2025-05-17 17:08:23 -0500

    Construccion Inicial del frontend

--- Program.cs
+++ Program.cs
@@ -1,70 +1,53 @@
-using MediReserva.Components;
+using Fluxor;
 using MediReserva.Data;
 using MediReserva.Middleware;
-using MediReserva.Services;
 using MediReserva.Services.Implementations;
 using MediReserva.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Syncfusion.Blazor;
 
-namespace MediReserva
+var builder = WebApplication.CreateBuilder(args);
+
+// Configuración de servicios
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ClinicaDB")));
+
+builder.Services.AddScoped<ICitaService, CitaService>();
+builder.Services.AddScoped<IEspecialidadService, EspecialidadService>();
+builder.Services.AddScoped<IMedicoService, MedicoService>();
+builder.Services.AddScoped<IPacienteService, PacienteService>();
+
+builder.Services.AddSyncfusionBlazor();
+builder.Services.AddFluxor(options => options.ScanAssemblies(typeof(Program).Assembly));
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+var app = builder.Build();
+
+app.UseMiddleware<ErrorHandlingMiddleware>();
+
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Configura el contexto de base de datos
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("ClinicaDB")));
-
-            // Inyección de dependencias para servicios de aplicación
-            builder.Services.AddScoped<ICitaService, CitaService>();
-            builder.Services.AddScoped<IEspecialidadService, EspecialidadService>();
-            builder.Services.AddScoped<IMedicoService, MedicoService>();
-            builder.Services.AddScoped<IPacienteService, PacienteService>();
-
-
-            // Registro de controladores API
-            builder.Services.AddControllers();
-
-            // Documentación Swagger para APIs
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            // Configuración de componentes Razor interactivos (Blazor Server)
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
-
-            var app = builder.Build();
-
-            // Middleware de manejo global de errores
-            app.UseMiddleware<ErrorHandlingMiddleware>();
-
-            // Configuración del pipeline HTTP
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
-            }
-            else
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseAntiforgery();
-
-            // Mapeo de componentes Blazor
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode();
-
-            // Mapeo de controladores API
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.MapControllers();
+
+app.Run();
