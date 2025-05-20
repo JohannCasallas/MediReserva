ae194225d114397a4dd942770b08070446acc7aa
Johann Casallas <casallasbecerrajohann@gmail.com>
2025-05-20 01:43:01 -0500

    Actualizaciones en el modal de Medico

--- Models/Medico.cs
+++ Models/Medico.cs
@@ -1,23 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
-namespace MediReserva.Models;
-
-public partial class Medico
+namespace MediReserva.Models
 {
-    public int Id { get; set; }
+    public partial class Medico
+    {
+        public int Id { get; set; }
 
-    public string Nombre { get; set; } = null!;
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100, ErrorMessage = "El nombre no puede exceder 100 caracteres")]
+        public string Nombre { get; set; } = null!;
 
-    public string? Email { get; set; }
+        [EmailAddress(ErrorMessage = "Debe ser un correo válido")]
+        [StringLength(100, ErrorMessage = "El correo no puede exceder 100 caracteres")]
+        public string? Email { get; set; }
 
-    public string? Telefono { get; set; }
+        [Phone(ErrorMessage = "Debe ser un número de teléfono válido")]
+        [StringLength(15, ErrorMessage = "El teléfono no puede exceder 15 caracteres")]
+        public string? Telefono { get; set; }
 
-    public string? Consultorio { get; set; }
+        [StringLength(50, ErrorMessage = "El consultorio no puede exceder 50 caracteres")]
+        public string? Consultorio { get; set; }
 
-    public int EspecialidadId { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Debe seleccionar una especialidad válida")]
+        public int EspecialidadId { get; set; }
 
-    public virtual ICollection<Citum> Cita { get; set; } = new List<Citum>();
+        public virtual ICollection<Citum> Cita { get; set; } = new List<Citum>();
 
-    public virtual Especialidad Especialidad { get; set; } = null!;
+        public virtual Especialidad Especialidad { get; set; } = null!;
+    }
 }
